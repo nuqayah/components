71f69fef5cbeb2af437074c2e2c6db8a6be1920c
Mustafa J <mustafa.0x@gmail.com>
2021-12-30 10:56:15 -0800

    add filterer

---
+++ src/filterer.js
@@ -0,0 +1,55 @@
+import {debounce} from '~/util/util.js'
+
+const strip_harakat = s => ('' + s).replace(/[ً-ْۖ-ٰۜ]/g, '')
+const multi_match_map = {ا: 'اأآإى', أ: 'أإءؤئ', ء: 'ءأإؤئ', ت: 'تة', ة: 'ةته', ه: 'هة', ى: 'ىاي', ي: 'يى'}
+const multi_match_re = RegExp(`[${Object.keys(multi_match_map).join('')}]`, 'g')
+function prep_ar_query(q) {
+    q = escape_regex(strip_harakat(q)).replace(/[ء-يّ]/g, '$&[ً-ْ]*')
+    // 'g' isn't usually needed since we only .test, but if highlighting we need 'g'
+    return RegExp(q.replace(multi_match_re, m => `[${multi_match_map[m]}]`), 'g')
+}
+
+export function filterer(el, props) {
+    let items
+    let matches
+    let prepare_query
+    let filter
+    let callback
+    function update(props) {
+        items = props.items || ([])
+        matches = props.matches || ([])
+        prepare_query = props.prepare_query || (q => /[ء-ي]/.test(q) ? prep_ar_query(q) : q)
+        filter = props.filter || ((item, q) => q instanceof RegExp ? q.test(item) : item.includes(q))
+        callback = props.callback || (() => {})
+    }
+    update(props)
+
+    function search(e) {
+        let query = e.target.value
+        if (!items.length) {
+            console.warn('No items')
+            return
+        }
+        if (!query) {
+            callback(items)
+            return
+        }
+
+        matches = []
+        const prepped_query = prepare_query(query)
+        for (let i = 0, l = items.length; i < l; i++) {
+            if (filter(items[i], prepped_query))
+                matches.push(items[i])
+        }
+        callback(matches)
+    }
+    const cb = debounce(search, 100)
+    el.addEventListener('input', cb)
+
+    return {
+        destroy() {
+            el.removeEventListener('input', cb)
+        },
+        update,
+    }
+}

694e3a21b8a4245444c81a8851e1d7d42d5ce758
Mustafa J <mustafa.0x@gmail.com>
2022-02-22 07:51:13 -0800

    Popover: minor cleanup

--- src/Popover.svelte
+++ src/Popover.svelte
@@ -17,16 +17,13 @@ let show = false
 const position_cont = (el, attach_to) => { if (wrapper) positioner(el, attach_to.getBoundingClientRect(), 'bottom') }
 async function toggle(e) {
     show = !show
-
     if (show) {
         await tick()
         position_cont(wrapper, e.target)
     }
-    else
-        hide_cont()
 }
 function hide_cont(e) {
-    if (e && wrapper && !wrapper.contains(e.target))
+    if (wrapper && !wrapper.contains(e.target))
         setTimeout(() => show = false, 50)
 }
 </script>

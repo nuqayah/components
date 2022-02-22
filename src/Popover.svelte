3313427a44cdab12e059a79793bf5cb80673a3c3
Mustafa J <mustafa.0x@gmail.com>
2022-02-21 19:35:28 -0800

    remove tooltip copy; add popover

---
+++ src/Popover.svelte
@@ -0,0 +1,32 @@
+<slot name=button {toggle}/>
+
+{#if show}
+  <div class="absolute z-10" transition:fly={{duration: 150, y: 10}} bind:this={wrapper}>
+    <slot/>
+  </div>
+{/if}
+
+<svelte:window on:resize={() => position_cont(wrapper, wrapper.previousElementSibling)} on:mousedown={hide_cont}/>
+
+<script>
+import positioner from 'positioner'
+
+let wrapper
+let show = false
+
+const position_cont = (el, attach_to) => { if (wrapper) positioner(el, attach_to.getBoundingClientRect(), 'bottom') }
+async function toggle(e) {
+    show = !show
+
+    if (show) {
+        await tick()
+        position_cont(wrapper, e.target)
+    }
+    else
+        hide_cont()
+}
+function hide_cont(e) {
+    if (e && wrapper && !wrapper.contains(e.target))
+        setTimeout(() => show = false, 50)
+}
+</script>

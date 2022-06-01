0b123b7c59382738cc3eb73440f5af3885517e88
Mustafa J <mustafa.0x@gmail.com>
2022-06-01 09:05:29 +0300

    filterer: remove "pass index to matches"

--- src/filterer.js
+++ src/filterer.js
@@ -43,7 +43,7 @@ export function filterer(el, props) {
         const prepped_query = prepare_query(query)
         for (let i = 0, l = items.length; i < l; i++) {
             if (filter(items[i], prepped_query))
-                matches.push([i, items[i]])
+                matches.push(items[i])
         }
         callback(matches)
     }

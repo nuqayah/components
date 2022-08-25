6b316ad5297b8418833ccff3a6556c41382c2b5f
Mustafa J <mustafa.0x@gmail.com>
2022-08-25 12:23:52 -0700

    popover: kbd fixes

--- src/Popover.svelte
+++ src/Popover.svelte
@@ -20,14 +20,19 @@ function position_cont() {
    if (shown && wrapper)
       positioner(wrapper, wrapper.previousElementSibling.getBoundingClientRect(), 'bottom')
 }
-async function show() {
-    if (shown)
+async function show(e) {
+    if (shown) {
+        if (!e.screenX)
+            shown = false // Only hide if kbd click; hide_cont takes care of mouse click.
         return
+    }
     shown = true
     await tick()
     position_cont()
 }
 function hide_cont(e) {
+    if (!e.screenX) // Vimium triggers mousedown
+        return
     const el = e.target
     // Even if hide_on_click is set, don't hide if the active element is an input
     const should_hide = shown && wrapper &&

cd462fa889f2fe3d516aad5fd408e6304034207e
Mustafa J <mustafa.0x@gmail.com>
2023-02-13 10:38:43 -0800

    toggle: add slot (will remove text prop later)

--- src/Toggle.svelte
+++ src/Toggle.svelte
@@ -1,4 +1,4 @@
-<label class="inline-flex items-center">{text}{text ? ' ' : ''}<input type=checkbox hidden bind:checked={value} on:change><div/></label>
+<label class="inline-flex items-center">{text}{text ? ' ' : ''}<slot/><input type=checkbox hidden bind:checked={value} on:change><div/></label>
 
 <script>
 export let value = false

7444c972752740b88be911c5dea6194aa52ac457
Mustafa J <mustafa.0x@gmail.com>
2023-02-16 11:06:54 -0800

    add ToggleSimple

---
+++ src/ToggleSimple.svelte
@@ -0,0 +1,47 @@
+<input type=checkbox hidden bind:checked={value} on:change><div/>
+
+<script>
+export let value = false
+</script>
+
+<style>
+label {
+  cursor: pointer;
+}
+div {
+  outline: 0;
+  width: 2.8rem;
+  height: 1.5rem;
+  position: relative;
+  cursor: pointer;
+  user-select: none;
+
+  background: var(--border-color, #ddd);
+  border-radius: 1.5rem;
+  padding: 2px;
+  transition: background 0.4s ease;
+  transform: scale(0.7);
+}
+div:after, div:before {
+  position: relative;
+  display: block;
+  content: "";
+  width: 50%;
+  height: 100%;
+}
+div:after {
+  inset-inline-start: 0%;
+  border-radius: 50%;
+  background: #fff;
+  transition: inset-inline-start 0.2s ease;
+}
+div:before {
+  display: none;
+}
+input[type=checkbox]:checked + div {
+  background: #009642;
+}
+input[type=checkbox]:checked + div:after {
+  inset-inline-start: 50%;
+}
+</style>

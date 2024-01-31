5eaf12a2f8d5c626833a6459d6150363f9f51dd4
Mustafa J <mustafa.0x@gmail.com>
2024-01-31 14:17:53 -0800

    add RequestAnim

---
+++ src/RequestAnim.svelte
@@ -0,0 +1,38 @@
+<div class="request-indicator" style="--bg: {color}" class:active />
+
+<script>
+export let active
+export let color = '#d8eedb'
+</script>
+
+<style>
+@keyframes elastic-indicator {
+    0% {
+        left: 0;
+        right: 100%;
+    }
+    55% {
+        left: 0;
+        right: 0;
+    }
+    100% {
+        left: 100%;
+        right: 0;
+    }
+}
+.request-indicator {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 5px;
+    z-index: 9999;
+}
+.request-indicator.active:before {
+    content: '';
+    position: absolute;
+    height: 100%;
+    background-color: var(--bg);
+    animation: elastic-indicator 2s infinite;
+}
+</style>

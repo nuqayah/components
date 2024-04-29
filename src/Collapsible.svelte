a2c4c31d558d14bcf8574f403a2bc263f9a6be9e
Mustafa J <mustafa.0x@gmail.com>
2024-04-29 07:41:05 +0300

    add Collapsible

---
+++ src/Collapsible.svelte
@@ -0,0 +1,10 @@
+<slot name=button show={() => shown = !shown} {shown}/>
+
+<div class="block w-full transition-[max-height] overflow-auto" style:max-height={shown ? max_height : '0'}>
+  <slot/>
+</div>
+
+<script>
+export let shown = false
+export let max_height
+</script>

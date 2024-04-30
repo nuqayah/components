f8a6a8092125d9b7b74ad970702a06e5add17857
Mustafa J <mustafa.0x@gmail.com>
2024-04-30 14:22:19 +0300

    Collapsible: pass `shown` to <slot/>

--- src/Collapsible.svelte
+++ src/Collapsible.svelte
@@ -1,7 +1,7 @@
 <slot name=button show={() => shown = !shown} {shown}/>
 
 <div class="block w-full transition-[max-height] overflow-auto" style:max-height={shown ? max_height : '0'}>
-  <slot/>
+  <slot {shown}/>
 </div>
 
 <script>

c7044f0d4826601d9b385d53ac1c09f5a6a21bb6
Yehya22 <yehya.alsager@gmail.com>
2024-07-27 21:37:41 +0300

    update ButtonUp

--- src/ButtonUp.svelte
+++ src/ButtonUp.svelte
@@ -1,25 +1,25 @@
-<svelte:window bind:scrollY={scrollHeight} />
-{#if showGotoTop}
+<svelte:window bind:scrollY={scroll_height} />
+
+{#if show_goto_top}
     <button
         class="fixed bottom-4 left-4 z-20 h-10 w-10 cursor-pointer rounded-full bg-[#a1a1a3] opacity-50 transition-colors hover:bg-[#b8b8bb] hover:text-white hover:opacity-100"
         on:click={() => scrollTo({top: 0, behavior: 'smooth'})}
         transition:fade={{duration: 300}}
     >
-        <CaretUp class="mx-auto " color="#fff" size={20} weight="bold" />
+        <ChevronUp class="mx-auto" color="#fff" size={20} />
     </button>
 {/if}
 
 <script>
-import CaretUp from 'phosphor-svelte/lib/CaretUp'
-import {fade} from 'svelte/transition'
+    import { ChevronUp } from 'lucide-svelte'
+    import {fade} from 'svelte/transition'
 
-export let showAtPixel = 500
-
-let scrollHeight = 0
-let showGotoTop = 0
-let lastScrollHeight = 0
-$: {
-    showGotoTop = scrollHeight < lastScrollHeight && scrollHeight > showAtPixel
-    lastScrollHeight = scrollHeight
-}
+    let show_at_pixel = 500
+    let scroll_height = $state(0)
+    let last_scroll_height = 0
+    let show_goto_top = $derived.by(() => {
+       const c = scroll_height < last_scroll_height && scroll_height > show_at_pixel
+       last_scroll_height = scroll_height
+       return c
+    })
 </script>

d04ba87c15615bfa680c00b6bfc6de5a2a819655
nureddinhaji <drnhaji@hotmail.com>
2025-05-06 11:50:41 +0300

    util: enhance repeat_click to track click count

--- src/util.js
+++ src/util.js
@@ -73,16 +73,20 @@ export function median(values) {
 }
 export function repeat_click(el, callback) {
     let repeat_interval
+    let click_count = 0
     function click_wrapper(e) {
         cleanup()
         if (window._useragent.ios) // Prevent text selection
             e.preventDefault()
-        callback()
-        repeat_interval = setInterval(() => callback(), 150)
+        callback(++click_count)
+        repeat_interval = setInterval(() => callback(++click_count), 150)
     }
     // Use instead of pointer* events since e.preventDefault doesn't work on them
     const [start_ev, end_ev] = window._useragent.is_touch ? ['touchstart', 'touchend'] : ['mousedown', 'mouseup']
-    const cleanup = () => clearInterval(repeat_interval)
+    const cleanup = () => {
+        clearInterval(repeat_interval)
+        click_count = 0
+    }
     el.addEventListener(start_ev, click_wrapper)
     document.documentElement.addEventListener(end_ev, cleanup)
     return {

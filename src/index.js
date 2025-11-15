667ee4512ff88baea081862da513017e90f60d62
abdel-17 <abdelrahmanbahaa@proton.me>
2025-11-15 17:01:42 +0200

    Revert "fix: remove svelte files from exports in node"
    This reverts commit 3492eaa4163e10b216b1ba0f4ff316d9bf78ad88.

--- src/index.js
+++ src/index.js
@@ -6,4 +6,14 @@ export {default as PlayButton} from './PlayButton.svelte'
 export {default as Popover} from './Popover.svelte'
 export {default as Tooltip} from './Tooltip.svelte'
 export {default as VirtualList} from './VirtualList.svelte'
+export {default as ar_slugify} from './ar_slugify.js'
+export {default as combobox} from './combobox.js'
+export {default as lazy_load} from './lazy_load.js'
 export * from './util.js'
+
+/** @typedef {import('./combobox.js').ComboboxProps} ComboboxProps */
+
+/**
+ * @template T
+ * @typedef {import('./lazy_load.js').LazyLoadProps<T>} LazyLoadProps
+ */
